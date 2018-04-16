72dbbb45b64891450c04d8e648f1541a873f5f74
Flarwalker <rfcross1207@gmail.com>
2018-04-16 11:57:25 -0400

    Initial Commit. Creating Node Server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require ("express");
+const bodyParser = require ("body-parser");
+const fs = require ("fs");
+const path = require ("path");
+
+let app = express ();
+
+app.use("/css", express.static(path.resolve(__dirname + "/css")));
+app.use("/html", express.static(path.resolve(__dirname + "/html")));
+app.use("/js", express.static(path.resolve(__dirname + "/js")));
+
+// Sends the Main HTML Pages
+app.get("/", function (req, res) {
+   res.send(fs.readFileSync(path.resolve(__dirname + "/home.html"), {encoding: "utf8"}));
+});
+
+// Sets node js to listen to port 8080.
+app.listen(8080, function() {
+	console.log('Server running on port 8080!');
+	console.log('Visit http://localhost:8080 to view.');
+});
