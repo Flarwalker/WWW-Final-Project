8ad7d70dad75d130e84f26c2039d89ff92b205ec
Flarwalker <rfcross1207@gmail.com>
2018-04-22 11:47:35 -0400

    Database Inplementation of ticket prices

--- server.js
+++ server.js
@@ -2,13 +2,43 @@ const express = require ("express");
 const bodyParser = require ("body-parser");
 const fs = require ("fs");
 const path = require ("path");
+const mysql = require ("mysql");
 
 let app = express ();
+
+let conn = mysql.createConnection({
+  host: "localhost",
+  port: "3306",
+  user: "www",
+  password: "www",
+  database: "wwwproject"
+});
+
+conn.connect (function (err) {
+  if (err) throw err;
+  console.log("Connected");
+});
 
 app.use("/css", express.static(path.resolve(__dirname + "/css")));
 app.use("/html", express.static(path.resolve(__dirname + "/html")));
 app.use("/js", express.static(path.resolve(__dirname + "/js")));
 app.use("/img", express.static(path.resolve(__dirname + "/img")));
+
+app.get("/prices", function (req, res) {
+  conn.query("SELECT price FROM tickets", function (err, result) {
+    if (err) {
+      throw err;
+    } else {
+      var prices = {};
+      prices.adult = result[0].price;
+      prices.senior = result[1].price;
+      prices.child = result[2].price;
+      prices.pass = result[3].price;
+
+      res.send(prices);
+    }
+  });
+});
 
 // Sends the Main HTML Pages
 app.get("/", function (req, res) {
