7674e11d9ff6586ebf91d06e6f88ce8cea40380a
Alek Martinez <martinez21736@gmail.com>
2018-04-29 22:31:44 -0400

    uncomment sql in server

--- server.js
+++ server.js
@@ -2,22 +2,22 @@ const express = require ("express");
 const bodyParser = require ("body-parser");
 const fs = require ("fs");
 const path = require ("path");
-//const mysql = require ("mysql");
+const mysql = require ("mysql");
 
 let app = express ();
 
-//let conn = mysql.createConnection({
-//  host: "localhost",
-//  port: "3306",
-//  user: "www",
-//  password: "www",
-//  database: "wwwproject"
-//});
-//
-//conn.connect (function (err) {
-//  if (err) throw err;
-//  console.log("Connected");
-//});
+let conn = mysql.createConnection({
+  host: "localhost",
+  port: "3306",
+  user: "www",
+  password: "www",
+  database: "wwwproject"
+});
+
+conn.connect (function (err) {
+  if (err) throw err;
+  console.log("Connected");
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
